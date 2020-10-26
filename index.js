e952ed11a94cc5d0319fde3386c5bf40a9a3fa56
I-Am-Lain <vetter.william@gmail.com>
2020-10-26 11:33:21 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(s) {
+  let hours = s.split(`:`)
+
+  if (parseInt(hours) < 12) {
+    return `Good Morning`
+  } else if (parseInt(hours) >= 17) {
+    return `Good Evening`
+  } else return 'Good Afternoon'
+}
+
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(s) {
+  document.getElementById('greeting').innerText = s
+}
